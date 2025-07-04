e7434f2f07ce5a3747da9c06ccf0499950d818ba
filip.tunguz <filipkralh11@gmail.com>
2025-07-04 16:11:38 +0200

    mobile side navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 type NavigationItem = {
 	label: string;
@@ -54,7 +55,51 @@ function DesktopNavigation({ pathname }: { pathname: string }) {
 }
 
 function MobileNavigation({ pathname }: { pathname: string }) {
-	return <ul className="xl:hidden">mob nav</ul>;
+	const [showSideNav, setShowSideNav] = useState(false);
+
+	return (
+		<>
+			<div className="xl:hidden" onClick={() => setShowSideNav(true)}>
+				mob nav
+			</div>
+			<SideNavigation
+				show={showSideNav}
+				pathname={pathname}
+				onClose={() => setShowSideNav(false)}
+			/>
+		</>
+	);
+}
+
+function SideNavigation({
+	show,
+	pathname,
+	onClose,
+}: {
+	show: boolean;
+	pathname: string;
+	onClose: () => void;
+}) {
+	if (!show) return <></>;
+
+	return (
+		<ul className="flex flex-col gap-4 py-8 px-12 fixed top-0 right-0 h-full bg-background">
+			{navigationItems.map((item: NavigationItem) => (
+				<li key={item.label}>
+					<Link
+						href={item.href}
+						className={`uppercase font-medium ${pathname === item.href ? 'text-primary underline' : 'hover:underline'}`}
+					>
+						{item.label}
+					</Link>
+				</li>
+			))}
+			<HireMeButton />
+			<span className="text-xl absolute top-4 right-4 leading-none" onClick={onClose}>
+				X
+			</span>
+		</ul>
+	);
 }
 
 function HireMeButton() {
