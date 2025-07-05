47d1cfb5a8366773ac0ea4b84bb96dfb1d30f54b
filip.tunguz <filipkralh11@gmail.com>
2025-07-05 13:28:16 +0200

    about heading part

--- src/sections/heading-section.tsx
+++ src/sections/heading-section.tsx
@@ -7,39 +7,55 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function HeadingSection() {
 	return (
-		<section className="relative h-[1000px]">
+		<section className="relative py-8 lg:py-32">
 			<Image
 				src="/coding-background.jpg"
 				alt="Coding Background"
 				fill
 				className="object-cover -z-10"
 			/>
-			<div className="-z-10 absolute top-0 left-0 size-full bg-gradient-to-b from-background via-background/50 to-background" />
-			<Narrower className="flex gap-4">
-				<div className="flex-1">
-					<h1>Filip Tunguz</h1>
-				</div>
+			<div className="-z-10 absolute top-0 left-0 size-full bg-gradient-to-b from-background via-background/60 to-background" />
+			<Narrower className="flex flex-col-reverse lg:flex-row gap-12">
+				<AboutHeadingPart />
 				<ProfileHeadingPart />
 			</Narrower>
 		</section>
 	);
 }
 
+function AboutHeadingPart() {
+	return (
+		<div className="flex-1 my-auto">
+			<span className="text-lg font-medium leading-none">Frontend Developer</span>
+			<h1 className="text-5xl lg:text-7xl lg:leading-22 text-white font-bold mb-4">
+				Hello I&#39;m
+				<br />
+				<span className="text-primary">Filip Tunguz</span>
+			</h1>
+			<p>
+				TODO: I am a frontend developer with a passion for creating beautiful and functional web
+				applications. I specialize in React, Next.js, and TypeScript, and I love building
+				user-friendly interfaces that provide a great user experience.
+			</p>
+		</div>
+	);
+}
+
 function ProfileHeadingPart() {
 	return (
 		<div className="flex-1">
-			<div className="flex flex-col w-max">
+			<div className="mx-auto lg:mr-0 lg:ml-auto flex flex-col w-max">
 				<div className="relative w-max">
 					<Image
 						width="300"
 						height="300"
 						src="/filip-tunguz.jpg"
 						alt="Filip Tunguz's picture"
-						className="rounded-full border-8 border-primary"
+						className="rounded-full border-8 border-primary lg:size-[300px] size-[250px]"
 					/>
-					<div className="absolute top-1/2 left-1/2 transform -z-10 -translate-x-1/2 -translate-y-1/2 size-2/3 bg-primary rounded-full animate-ping"></div>
+					<div className="absolute top-1/2 left-1/2 transform -z-10 -translate-x-1/2 -translate-y-1/2 size-2/3 bg-primary rounded-full animate-ping" />
 				</div>
-				<div className="-mt-[150px] pt-[174px] pb-6 px-4 bg-background rounded-b-3xl flex flex-col gap-4 items-center">
+				<div className="-mt-[150px] pt-[174px] pb-6 px-4 bg-background border border-secondary/40 rounded-b-3xl flex flex-col gap-4 items-center">
 					<Button variant="outline" className="uppercase !px-6 !py-5 mb-2">
 						Download CV <MdOutlineFileDownload />
 					</Button>
