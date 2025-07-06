1bb5420761b019879254a215ca1b8fe6077db8e7
filip.tunguz <filipkralh11@gmail.com>
2025-07-06 12:46:47 +0200

    send email post request route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,23 +1,33 @@
 import { Resend } from 'resend';
+import { NextRequest } from 'next/server';
 import { EmailTemplate } from '@/components/email-template';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export type EmailData = {
+	fullName: string;
+	from: string;
+	subject: string;
+	message: string;
+};
+
+export async function POST(request: NextRequest) {
+	const payload = (await request.json()) as EmailData;
+
 	try {
 		const { data, error } = await resend.emails.send({
-			from: 'Acme <onboarding@resend.dev>',
-			to: ['delivered@resend.dev'],
-			subject: 'Hello world',
-			react: EmailTemplate({ firstName: 'John' }),
+			from: payload.from,
+			to: ['filiptunguz.su@gmail.com'],
+			subject: payload.subject,
+			react: EmailTemplate({ fullName: payload.fullName, message: payload.message }),
 		});
 
 		if (error) {
-			return Response.json({ error }, { status: 500 });
+			return Response.json({ error });
 		}
 
 		return Response.json(data);
 	} catch (error) {
-		return Response.json({ error }, { status: 500 });
+		return Response.json({ error });
 	}
 }
