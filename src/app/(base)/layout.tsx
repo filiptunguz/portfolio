e790331ba6d50eee39bab24e63be9b8725b096e6
filip.tunguz <filipkralh11@gmail.com>
2025-07-06 14:56:10 +0200

    enable toaster

--- src/app/(base)/layout.tsx
+++ src/app/(base)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/header';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function BaseLayout({
 	children,
@@ -9,6 +10,7 @@ export default function BaseLayout({
 		<>
 			<Header />
 			{children}
+			<Toaster />
 		</>
 	);
 }
