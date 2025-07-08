3d69fe2c24f1b55ea7493e79f8204c8fd298aa9a
filip.tunguz <filipkralh11@gmail.com>
2025-07-08 21:44:18 +0200

    change project card style

--- src/sections/projects-section.tsx
+++ src/sections/projects-section.tsx
@@ -9,11 +9,11 @@ export default function ProjectsSection() {
 				<h2 id="projects">My Projects</h2>
 				<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
 					<ProjectCard
-						title="Project Title"
-						src="/assets/project-1.jpg"
-						alt="Projekat"
-						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-						href="/filip-tunguz-portfolio"
+						title="Image Cropper"
+						src="/assets/image-cropper/image-preview-component.webp"
+						alt="Image Cropper Cover Image"
+						description="A lightweight React + TypeScript image upload library built with Tailwind. Includes drag-and-drop or file select, multi/single file support, image preview with removal and cropping, and all logic exposed through hooks for full control. Cropped images are ready-to-upload files, handled entirely on the frontend. Use built-in components or create your own easily."
+						href="/image-cropper"
 					/>
 					<ProjectCard
 						title="Project Title"
@@ -56,19 +56,24 @@ function ProjectCard({
 	href: string;
 }) {
 	return (
-		<div className="border border-secondary/40 p-6 rounded-3xl relative group cursor-pointer overflow-hidden space-y-4 mix-blend-lighten">
+		<Link
+			href={href}
+			className="border border-secondary p-6 rounded-3xl relative group cursor-pointer overflow-hidden h-[270px] space-y-4 flex items-end"
+		>
 			<Image
 				src={src}
 				alt={alt}
 				fill
 				className="object-cover -z-10 group-hover:scale-110 transition-[scale]"
 			/>
-			<div className="absolute top-0 left-0 size-full -z-10 group-hover:backdrop-blur-md transition-all bg-gradient-to-r from-background/90 to-background/70" />
-			<h3 className="text-xl font-bold leading-none">{title}</h3>
-			<p className="text-sm">{description}</p>
-			<Link href={href} className="text-primary hover:underline underline-offset-4">
-				View Project
-			</Link>
-		</div>
+			<div className="opacity-0 group-hover:opacity-100 absolute top-0 left-0 size-full -z-10 transition-opacity bg-gradient-to-r from-background/90 to-background/70" />
+			<div className="max-h-6 group-hover:max-h-[270px] group-hover:backdrop-blur transition-[max-height,backdrop-filter] overflow-hidden space-y-4 duration-400">
+				<h3 className="text-xl font-bold leading-none drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">
+					{title}
+				</h3>
+				<p className="text-sm">{description}</p>
+				<span className="text-primary hover:underline underline-offset-4">View Project</span>
+			</div>
+		</Link>
 	);
 }
