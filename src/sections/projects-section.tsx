9b9125c5f549a5fe772fae3e16e79ab525e999e8
filip.tunguz <filipkralh11@gmail.com>
2025-07-09 12:05:50 +0200

    refactor projects section to dynamically render project cards from database

--- src/sections/projects-section.tsx
+++ src/sections/projects-section.tsx
@@ -1,6 +1,7 @@
 import Narrower from '@/components/narrower';
 import Image from 'next/image';
 import Link from 'next/link';
+import { projects } from '@/database/projects';
 
 export default function ProjectsSection() {
 	return (
@@ -8,34 +9,16 @@ export default function ProjectsSection() {
 			<Narrower className="space-y-8">
 				<h2 id="projects">My Projects</h2>
 				<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-					<ProjectCard
-						title="Image Cropper"
-						src="/assets/image-cropper/image-preview-component.webp"
-						alt="Image Cropper Cover Image"
-						description="A lightweight React + TypeScript image upload library built with Tailwind. Includes drag-and-drop or file select, multi/single file support, image preview with removal and cropping, and all logic exposed through hooks for full control. Cropped images are ready-to-upload files, handled entirely on the frontend. Use built-in components or create your own easily."
-						href="/image-cropper"
-					/>
-					<ProjectCard
-						title="Project Title"
-						src="/assets/project-2.jpg"
-						alt="Projekat"
-						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-						href="/project-2"
-					/>
-					<ProjectCard
-						title="Project Title"
-						src="/assets/project-2.jpg"
-						alt="Projekat"
-						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-						href="/project-3"
-					/>
-					<ProjectCard
-						title="Project Title"
-						src="/assets/project-2.jpg"
-						alt="Projekat"
-						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-						href="/project-4"
-					/>
+					{projects.map((project) => (
+						<ProjectCard
+							key={project.slug}
+							title={project.title}
+							src={project.coverImage}
+							alt={`${project.title} cover image`}
+							description={project.description}
+							href={project.slug}
+						/>
+					))}
 				</div>
 			</Narrower>
 		</section>
