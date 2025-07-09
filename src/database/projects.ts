2c6e4c7d99981a486d45072f1afe57bc43423075
filip.tunguz <filipkralh11@gmail.com>
2025-07-09 12:01:49 +0200

    isochrone reachzone project

--- src/database/projects.ts
+++ src/database/projects.ts
@@ -27,7 +27,7 @@ export const projects: Project[] = [
 		coverImage: '/assets/image-cropper/image-preview-component.webp',
 		githubUrl: 'https://github.com/filiptunguz/image-uploader',
 		mainImage: '/assets/image-cropper/image-cropper.gif',
-		technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Vite'],
+		technologies: ['React', 'JavaScript', 'TypeScript', 'Tailwind CSS', 'Vite'],
 		screenshots: [
 			{
 				title: 'Drag and Drop Hook and Component',
@@ -64,5 +64,49 @@ export const projects: Project[] = [
 		metaTitle: 'Image Cropper',
 		metaDescription:
 			'This component enables image uploading through drag-and-drop or file selection. It supports multiple image uploads, real-time previews, and a (for now) image cropping feature.',
+	},
+	{
+		title: 'Isochrone Reachzone',
+		slug: 'isochrone-reachzone',
+		description:
+			'An interactive React + Vite + TypeScript + Tailwind component showcasing isochrone generation on a map. Users can select transport type (walk, drive, bus, bike) and travel time via a simple form. The map dynamically updates to display the reachable area (isochrone) from a fixed starting point, centering and adjusting the view on each change.',
+		coverImage: '/assets/isochrone-reachzone/full-app.webp',
+		githubUrl: 'https://github.com/filiptunguz/isochrone-reachzone',
+		mainImage: '/assets/isochrone-reachzone/isochrone-reachzone.gif',
+		technologies: ['React', 'JavaScript', 'TypeScript', 'Tailwind CSS', 'Vite', 'Leaflet'],
+		screenshots: [
+			{
+				title: 'Form',
+				src: '/assets/isochrone-reachzone/form.webp',
+				alt: 'Form Component',
+				points: [
+					'A responsive form allowing users to select one of four transport modes (walk, drive, bus, bike) and adjust travel time using a slider.',
+					'Each change instantly updates the isochrone map below, providing an intuitive and interactive way to visualize reachable areas based on selected travel parameters.',
+				],
+			},
+			{
+				title: 'OpenStreetMap',
+				src: '/assets/isochrone-reachzone/full-app.webp',
+				alt: 'OpenStreetMap Component',
+				points: [
+					'An interactive map rendered with Leaflet via React-Leaflet, displaying geographic data using OpenStreetMap tiles.',
+					'It provides smooth user interactions like zooming and panning, while integrating custom overlays and markers.',
+					'The map efficiently responds to state changes, offering clear visual feedback and seamless integration with external UI controls.',
+				],
+			},
+			{
+				title: 'Isochrone Polygon',
+				src: '/assets/isochrone-reachzone/polygon.webp',
+				alt: 'Isochrone Polygon Component',
+				points: [
+					'A dynamic polygon rendered with React-Leaflet’s Polygon component to visualize isochrone boundaries on the map.',
+					'It updates in real-time based on user input, accurately outlining the area reachable within the selected travel conditions.',
+					'The polygon is styled for clarity and overlays seamlessly on the map without performance issues.',
+				],
+			},
+		],
+		metaTitle: 'Isochrone Reachzone',
+		metaDescription:
+			'An interactive React + Vite + TypeScript + Tailwind component showcasing isochrone generation on a map. Users can select transport type (walk, drive, bus, bike) and travel time via a simple form. The map dynamically updates to display the reachable area (isochrone) from a fixed starting point, centering and adjusting the view on each change.',
 	},
 ];
