db29ff520104a14e246458fa26bb85521b06feac
filip.tunguz <filipkralh11@gmail.com>
2025-07-11 14:07:13 +0200

    favicon and og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ export const metadata: Metadata = {
 	title: 'Filip Tunguz - Frontend Developer',
 	description:
 		'Frontend developer with 4 years of experience, specializing in React, Next.js, and TypeScript, with a strong focus on performance and scalable architecture. I enjoy building user-friendly interfaces with Tailwind and am currently expanding into mobile development using React Native with Expo for cross-platform consistency.',
+	icons: {
+		apple: '/assets/favicon/apple-touch-icon.png',
+		icon: [
+			{ rel: 'icon', type: 'image/png', sizes: '32x32', url: '/assets/favicon/favicon-32x32.png' },
+			{ rel: 'icon', type: 'image/png', sizes: '16x16', url: '/assets/favicon/favicon-16x16.png' },
+		],
+	},
+	manifest: '/assets/favicon/site.webmanifest',
+	openGraph: {
+		images: '/assets/filiptunguz_og_image.jpg',
+	},
+	twitter: {
+		images: '/assets/filiptunguz_og_image.jpg',
+	},
 };
 
 export default function RootLayout({
